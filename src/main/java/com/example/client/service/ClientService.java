cdd874055d62fbb0a98adc258771b340e0a266c6
HP <anaflousmimouna@gmail.com>
2022-02-07 16:33:19 +0100

    Initial commit

---
+++ src/main/java/com/example/client/service/ClientService.java
@@ -0,0 +1,53 @@
+package com.example.client.service;
+
+
+import com.example.client.exception.ClientNotFoundException;
+import com.example.client.model.Client;
+import com.example.client.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClientService {
+    @Autowired
+	private final ClientRepository clientRepository;
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+    public List<Client> findAllClients(){
+        return clientRepository.findAll();
+    }
+    public Client addClient(Client client){         
+    	client.setPassword(new BCryptPasswordEncoder().encode(client.getPassword()));
+    	client.setRole("Client");
+    	return clientRepository.save(client);
+    }
+    public Client getClientByEmail(String email){
+        return  clientRepository.findClientByEmail(email);
+    }
+
+    public Client findClientBynumGSM(String numGSM){
+        return clientRepository.findBynumGSM(numGSM);
+    }
+    public Client findClientById(Long id){
+        return clientRepository.findByClientId(id);
+    }
+    public Client findClientByCin(String cin){
+        return clientRepository.findBycin(cin);
+    }
+    public Client updateClient(Long id,Client client)throws ClientNotFoundException {
+    	Client clientFromDB = clientRepository.findByClientId(id);
+        if (clientFromDB == null)
+            throw new ClientNotFoundException("aucun client avec cette id");
+        client.setClientId(clientFromDB.getClientId());
+        client.setPassword(new BCryptPasswordEncoder().encode(client.getPassword()));
+        return clientRepository.save(client);
+    }
+    
+    
+   
+}
